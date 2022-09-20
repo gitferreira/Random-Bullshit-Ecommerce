1f687ed2284525ad6b13040596cb6ea560c0d3d0
gitferreira <iniferre96@gmail.com>
2022-09-20 12:57:01 +0200

    almost done

--- components/Block.js
+++ components/Block.js
@@ -1,15 +1,30 @@
 
+import styles from '../styles/Block.module.css'
+
 const Block = ({blocks}) => {
-    const {heading, content, Image} = blocks
+    const {Heading, Content, Image} = blocks
   return (
     <section> 
-<div className="contenedor">
+<div className={`contenedor ${styles.grid}`}>
    
-    <div>
-        <h2></h2>
-        <p></p>
+    <div className={styles.contenido}>
+        <h2 className='heading'>{Heading}</h2>
+        <p className={styles.text}>{Content}</p>
     </div>
 </div>
+<style jsx>{
+  `
+  section {
+    padding: 10rem 0;
+    margin-top: 10rem;
+    background-image: linear-gradient(to right, rgb(0 0 0 / .65), rgb(0 0 0 / .7)), url(${ Image.url});
+    background-size: cover;
+    background-position: 50%;
+  }
+  `
+}
+
+</style >
 
     </section>
   )
--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,24 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/dist/client/link";
 import Layout from "../components/Layout";
-import List from  "../components/List"
-import Block from  "../components/Block"
+import List from "../components/List";
+import Block from "../components/Block";
+import ListBlog from "../components/ListBlog";
 
-export default function Home({products, blocks}) {
+export default function Home({ products, blocks, entries }) {
   return (
-    <Layout page ="Home">
-<main className="contenedor">
-  <h1 className="heading"> Our Products </h1>
-  <List products = {products} />
-</main>
-<Block 
-  blocks = {blocks}
-/>
+    <Layout page="Home">
+      <main className="contenedor">
+        <h1 className="heading"> Our Products </h1>
+        <List products={products} />
+      </main>
+      <Block blocks={blocks} />
+      <section className="contenedor">
+        <ListBlog entries={entries} />
+      </section>
     </Layout>
   );
 }
-
 
 export async function getServerSideProps() {
   // const url = `${process.env.API_URL}/products?_sort=created_at:desc`;
@@ -29,26 +30,29 @@ export async function getServerSideProps() {
   // const response = await fetch(url)
   // const blocks = await response.json()
 
-
   //MULTIPLE ASYNC CALLS
   const urlProducts = `${process.env.API_URL}/products?_sort=created_at:desc`;
   const urlBlocks = `${process.env.API_URL}/blocks`;
+  const urlBlogs = `${process.env.API_URL}/blogs?_limit=3&_sort=created_at:desc`;
 
-  const [resProducts, resBlocks] = await Promise.all([
+  const [resProducts, resBlocks, resBlogs] = await Promise.all([
     fetch(urlProducts),
     fetch(urlBlocks),
-  ])
+    fetch(urlBlogs),
+  ]);
 
-  const [products, blocks] = await Promise.all([
+  const [products, blocks, entries] = await Promise.all([
     resProducts.json(),
-    resBlocks.json()
-  ])
+    resBlocks.json(),
+    resBlogs.json(),
+  ]);
 
   return {
     props: {
       //Due to eS6 and object literal enhancement there is no need to specify : products
       products,
-      blocks
-    }
-  }
+      blocks,
+      entries,
+    },
+  };
 }
